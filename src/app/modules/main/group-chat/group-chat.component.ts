2974fc3ee5d6ec5f2ca718190e5354bfbaa304b4
Jakob-Niklas See <blendercraft.info@gmail.com>
2019-09-28 12:56:50 +0200

    Component structure

---
+++ src/app/modules/main/group-chat/group-chat.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-group-chat',
+    templateUrl: './group-chat.component.html',
+    styleUrls: ['./group-chat.component.scss']
+})
+export class GroupChatComponent implements OnInit
+{
+    constructor()
+    {
+    }
+
+    ngOnInit(): void
+    {
+    }
+}
