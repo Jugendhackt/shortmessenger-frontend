32ea0ba027e102923f03d9cbcd0e7c38cd8ffc88
niklasApps <privat@nschroetler.de>
2019-09-28 15:08:39 +0200

    Added send command for api

--- src/app/core/api/api.service.ts
+++ src/app/core/api/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Chat } from './interfaces/Chat.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
@@ -21,12 +22,28 @@ export class Api {
 		});
 	}
 
-	read() {
+	read(offset:number = 0) {
 		var params = new HttpParams()
 			.set("action", "read")
 			.set("username", "admin")
 			.set("password", "admin")
 
 		return this.http.get<Array<Chat>>(this.apiUrl, {params})
+	}
+
+	send(chatId, message) {
+		var params = new HttpParams()
+			.set("action", "send")
+			.set("username", "admin")
+			.set("password", "admin")
+			.set("chatId", chatId)
+			.set("message", message)
+
+		var body = {
+			chatId: chatId,
+			message: message
+		}
+
+		return this.http.post(this.apiUrl, body, {params})
 	}
 }
